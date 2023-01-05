c69aebe104379b5b7f3e41080d2d73d75c896cef
Davi Fernandes <davifernandes507@gmail.com>
2023-01-04 22:50:44 -0300

    ✨ Added Table of courses

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { CourseType } from './../model/course';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
 
+
+  courses: CourseType[] = [
+    { _id: '1', name: 'Angular', category: 'FrontEnd' },
+    { _id: '2', name: 'Spring', category: 'BackEnd' }
+  ];
+  displayedColumns: string[] = ['name', 'category'];
+
+  constructor() {
+    // this.courses = []
+  }
+
+  ngOnInit(): void {
+
+  }
 }
---
+++ src/app/courses/model/course.ts
@@ -0,0 +1,5 @@
+export type CourseType = {
+  _id: string,
+  name: string,
+  category: string,
+}
